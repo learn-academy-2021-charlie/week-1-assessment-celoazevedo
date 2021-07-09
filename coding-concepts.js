31ea513eb43fc97a83ae30403c75b90f31430084
celoazevedo <meazevedo86@gmail.com>
2021-07-09 14:44:54 -0700

    complete concepts question 2

--- coding-concepts.js
+++ coding-concepts.js
@@ -11,7 +11,7 @@ var cohort = "Charlie 2021"
 console.log(cohort.length)
 
 // a) Your answer: the output will be the number of characters in the string. In this case 12 (count the space as a character).
-// b) Verify and explain: when you apply the length property on a string the output will be the number of characters on that string.
+// b) Verify and explain: when you apply the length property on a string the output will be the number of characters on that string (including spaces).
 
 
 // --------------------2) What will this log?
@@ -20,7 +20,8 @@ var greeting = "Hello World!"
 console.log(greeting[3])
 
 // a) Your answer: it will log the value of the character at the index 3 position. In this case it will be the letter 'l'.
-// b) Verify and explain:
+// b) Verify and explain: the square braket notation when applied to the variable that was assigined to the 'Hello World!' string, 
+//    it gives access to the character of that string located at the index position used inside of the squre brakets.
 
 
 // --------------------3) What will this log?
