426edd3679323100490bedf9566476efa59e7a43
celoazevedo <meazevedo86@gmail.com>
2021-07-11 10:08:18 -0700

    minor edits to concepts questions

--- coding-concepts.js
+++ coding-concepts.js
@@ -32,7 +32,7 @@ console.log(languages[index])
 
 // a) Your answer: it will log the element in the array located in the index position 1. In this case a string with a value 'Ruby'.
 // b) Verify and explain: We can access elements in an array using the same braket notation that we used in the question above with the string data type.
-//    In the scenarion above, we are declaring 2 variables. In one we are assiging an array with 4 elements and the other the value 1.
+//    In the scenarion above, we are declaring 2 variables. In one we are assiging an array with 4 elements and in the other the value 1.
 //    The variable index is assigned a value of 1. And the index variable with a value of 1 is being used inside the 
 //    braket notation to access the element located at the index 1 position in the array, which is the second element in the array ('Ruby').
 
@@ -42,14 +42,14 @@ console.log(languages[index])
 var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer: it will log the weekendDays array will the string elements inside with all of its letters converted to large uppercase letters. ["SATURDAY", "SUNDAY"]
+// a) Your answer: it will log the weekendDays array with the string elements inside with all of its letters converted to large uppercase letters. ["SATURDAY", "SUNDAY"]
 // b) Verify and explain: I was wrong. Here is my error: console.log(weekendDays.toUpperCase())
                         // ^
 
                         // TypeError: weekendDays.toUpperCase is not a function
 //    My understanding now is that toUpperCase() is a string method and not and an array method. In order to use
 //    the toUpperCase() method to convert the letters of the string elements in the array, we need to access the individual elements (which in our case are strings)
-//    and apply the method to each element. To do this we need to iterate through the array using a forloop of we could also use the higher order functions .map() or .forEach
+//    and apply the method to each element. To do this we need to iterate through the array using a forloop or we could also use the higher order functions .map() or .forEach().
 
 
 // --------------------5) What will this log?
@@ -58,6 +58,6 @@ var dataTypes = ["number", "string", "Boolean"]
 console.log(typeof dataTypes.length)
 
 // a) Your answer: it will log the data type of the result from when you apply the length property to the dataTypes array. In this case the array
-//    have 3 elements so dataTypes.length will be equal the number 3. So the console should log the string 'number'.
+//    have 3 elements so dataTypes.length will be equal to the number 3. So the console should log the string 'number'.
 // b) Verify and explain: the .length property when applied to an array or a string will output data of type number that represents the number of elements in the array
 //    or number of characters in a string.
